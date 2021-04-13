7ab2c94f8154f7bb525697639addd12a73b51455
katan4722 <uver7770422@gmail.com>
2021-04-13 22:59:33 +0900

    ♻️ WORKS部分の整形

--- components/works.js
+++ components/works.js
@@ -6,16 +6,34 @@ const WORKS = [
     title: "ポートフォリオ",
     img: "/images/cafe.png",
   },
+  {
+    url: "",
+    title: "ポートフォリオ",
+    img: "/images/cafe.png",
+  },
+  {
+    url: "",
+    title: "ポートフォリオ",
+    img: "/images/cafe.png",
+  },
 ];
-
 
 export function Works() {
   return (
     <div className={classes.works}>
       <h1>WORKS</h1>
-      {WORKS.map((work) => {
-        return <img src={work.img} alt={work.title} />;
-      })}
+      <div className={classes.container}>
+        {WORKS.map((work) => {
+          return (
+            <div className={classes.work}>
+              <img src={work.img} alt={work.title} className={classes.img} />
+              <p>
+                説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト説明テキスト
+              </p>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
